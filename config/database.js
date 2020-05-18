cd0cba34e6590c5db2f5bde6ce855633d37232a6
Eugene-Okhryamkin <eugene.okhryamkin@gmail.com>
2020-05-18 11:38:39 +0300

    db config

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
 const Sequelize = require("sequelize");
 
-// const db = new Sequelize(process.env.DB, process.env.USER, process.env.PASSWORD, {
-//     host: process.env.HOST,
-//     dialect: "mysql"
-// });
-
-const db = new Sequelize("med", "root", "qwe123", {
-    host: "localhost",
+const db = new Sequelize(process.env.DB, process.env.USER, process.env.PASSWORD, {
+    host: process.env.HOST,
     dialect: "mysql"
 });
+
+// const db = new Sequelize("med", "root", "qwe123", {
+//     host: "localhost",
+//     dialect: "mysql"
+// });
 
 db.sync();
 
